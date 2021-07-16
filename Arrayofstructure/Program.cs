493ed0213a5c2fd42ee5fd7e6bbf674a41083adb
Arunnathb <76033132+Arunnathb@users.noreply.github.com>
2021-07-16 14:25:42 +0530

    test file

--- Arrayofstructure/Program.cs
+++ Arrayofstructure/Program.cs
@@ -199,7 +199,7 @@ namespace newRTECreation
                   //int q = 6;
                   // int z = 8;
                 
-               for(int ires=0;ires<3;ires++)
+               for(int ires=0;ires<3;ires++)   // struct size
                 {
                     int l = 0;
                     int m = 1;
